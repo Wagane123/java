0fdffe037c4789d426655ea4ed707b6dac652fcf
Wagane123 <elhadjiwaganefaye433@gmail.com>
2023-07-30 18:29:36 +0200

    Modification class Cours

--- src/Gestion_Note/Cours.java
+++ src/Gestion_Note/Cours.java
@@ -2,9 +2,8 @@ package Gestion_Note;
 
 public class Cours {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	//public static void main(String[] args) {
 
-	}
+	//}
 
 }
